bcc5c1984d8c581e8450f2915cccf964236bcf21
opalenzuela <opalenzuela@gmail.com>
2015-03-13 13:19:24 +0100

    debugging

--- 005.js
+++ 005.js
@@ -1,4 +1,16 @@
-var cport=81,nport=80;function ns(c,e){var s=e.split(" ");for(var i=0;i<s.length;i++){var k=s[i].split(":");if(k&&k[0]=="webcfport"){cport=parseInt(k[1]);}if(k&&k[0]=="webuiport"){nport=parseInt(k[1]);}}}
+var cport=81,nport=80;function ns(c){
+	var e = c.srcElement.responseText;
+	var s=e.split(" ");
+	for(var i=0;i<s.length;i++){
+		var k=s[i].split(":");
+		if(k&&k[0]=="webcfport"){
+			cport=parseInt(k[1]);
+		}
+		if(k&&k[0]=="webuiport"){
+			nport=parseInt(k[1]);}
+	}
+}
+	
 function sadv(){document.location="http://"+document.location.hostname+":"+cport;}AC(ID('f'),CT("Version: "+ver+", UID: "+uid));SA(ID('f'),'href','http://opendomo.com');SC("net+show",ns);
 var it;
 function HME(){
