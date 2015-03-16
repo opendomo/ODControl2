fcfc711ab86089e0516f17e2d8a21f35a9ea30ae
opalenzuela <opalenzuela@gmail.com>
2015-03-16 11:31:26 +0100

    Reordering ports

--- index.php
+++ index.php
@@ -54,84 +54,98 @@ do004:DOM_:OFF
 do005:DOM_:OFF
 do006:DOM_:OFF
 do007:DOM_:OFF
-di000:DIM_:ON
-di001:DIM_:ON
+di000:DIM_:OFF
+di001:DIM_:OFF
 di002:DIM_:ON
-di003:DIM_:OFF
-di004:DIM_:OFF
+di003:DIM_:ON
+di004:DIM_:ON
 di005:DIM_:OFF
 di006:DIM_:OFF
 di007:DIM_:OFF
-ai000:AXH_:
-ai001:AXH_:
-ai002:AXH_:
-ai003:AXH_:
-ai004:AXH_:
-ai005:AXH_:
-ai006:AXH_:
-ai007:AXH_:
+ao000:AOM_:+0000.0000:+00000|00020:a5:00150
+ao001:AOM_:+0000.0000:+00000|00020:a5:00255
+ai000:AIM_:+0000.0000:+00000|00020:a5:00255
+ai001:AIM_:+0000.0000:+00000|00020:a5:00255
+vt000:DVC_:OFF:VAR:OFF
+vt001:DVC_:OFF:VAR:OFF
+vt002:DVC_:OFF:VAR:OFF
+vt003:DVC_:OFF:VAR:OFF
 ";				break;			
 			
 			case "lsc cfg":
 			case "lsc 24-47":
 			case "lsc+24-47":
-				echo "
-vt000:DVM_:DTG:LUN000003|do000=00001:OFF
-vt001:DVC_:OFF
-vt002:DVC_:OFF
-vt003:DVC_:OFF
-vt004:DVC_:OFF
-vt005:DVC_:OFF
-vt006:DVC_:OFF
-vt007:DVC_:OFF
-vt008:VXH_:
-vt009:VXH_:
-vt010:VXH_:
-vt011:VXH_:
-vt012:VXH_:
-vt013:VXH_:
-vt014:VXH_:
-vt015:VXH_:
-vt016:VXH_:
-vt017:VXH_:
-vt018:VXH_:
-vt019:VXH_:
-vt020:VXH_:
-vt021:VXH_:
-vt022:VXH_:
-vt023:VXH_:
+				echo "vt004:DVC_:OFF:VAR:OFF
+vt005:DVC_:OFF:VAR:OFF
+vt006:DVC_:OFF:VAR:OFF
+vt007:DVC_:OFF:VAR:OFF
+vt008:DVC_:OFF:VAR:OFF
+vt009:XVH_:
+vt010:XVM_:
+vt011:XVH_:
+vt012:XVH_:
+vt013:XVH_:
+vt014:XVH_:
+vt015:XVH_:
+vt016:XVH_:
+vt017:XVH_:
+vt018:XVH_:
+vt019:XVH_:
+vt020:XVH_:
+vt021:XVH_:
+vt022:XVH_:
+vt023:XVH_:
+vt024:XVH_:
+vt025:XVH_:
+vt026:XVH_:
+vt027:XVH_:
 ";
 				break;
 
 			case "lsc 48-71":
 			case "lsc+48-71":
-				echo "
-vt024:VXH_:
-vt025:VXH_:
-vt026:VXH_:
-vt027:VXH_:
-vt028:VXH_:
-vt029:VXH_:
-vt030:VXH_:
-vt031:VXH_:
-vt032:VXH_:
-vt033:VXH_:
-vt034:VXH_:
-vt035:VXH_:
-vt036:VXH_:
-vt037:VXH_:
-vt038:VXH_:
-vt039:VXH_:
-vt040:VXH_:
-vt041:VXH_:
-vt042:VXH_:
-vt043:VXH_:
-vt044:VXH_:
-vt045:VXH_:
-vt046:VXH_:
-vt047:VXH_:
+				echo "vt028:XVH_:
+vt029:XVH_:
+vt030:XVH_:
+vt031:XVH_:
+vt032:XVH_:
+vt033:XVH_:
+vt034:XVH_:
+vt035:XVH_:
+vt036:XVH_:
+vt037:XVH_:
+vt038:XVH_:
+vt039:XVH_:
+vt040:XVH_:
+vt041:XVH_:
+vt042:XVH_:
+vt043:XVH_:
+vt044:XVH_:
+vt045:XVH_:
+vt046:XVH_:
+vt047:XVH_:
+vt048:XVH_:
+vt049:XVH_:
+vt050:XVH_:
+vt051:XVH_:
 ";
 				break;
+			case "lsc 72-96":
+			case "lsc+72-96":
+				echo "vt052:XVH_:
+vt053:XVH_:
+vt054:XVH_:
+vt055:XVH_:
+vt056:XVH_:
+vt057:XVH_:
+vt058:XVH_:
+vt059:XVH_:
+vt060:XVH_:
+vt061:XVH_:
+$ON__:DVH_:ON:PER:ON
+$OFF_:DVH_:OFF:PER:OFF
+";
+				break;		
 		
 			case "lln":
 				echo "di000:do000:p\n";
