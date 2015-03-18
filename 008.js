6d8b099507df0c2270a44ab0c8393c1793580a7b
opalenzuela <opalenzuela@gmail.com>
2015-03-18 13:13:45 +0100

    removing localStorage

--- 008.js
+++ 008.js
@@ -1,3 +1,3 @@
 function TB(i,l,f) {AC(ID('h'),CE('a','cat',i));AC(ID(i),CT(l));ID(i).href='javascript:'+f+'();';}
-function RL(){localStorage.clear();D.location.reload();}
-localStorage["frequency"]="8000";
+function RL(){D.location.reload();}
+/*localStorage["frequency"]="8000"; */
