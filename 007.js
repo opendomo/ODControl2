bc5b99b1e6e6176f9be22fb35b5e0ce90a13cf18
opalenzuela <opalenzuela@gmail.com>
2015-03-19 15:19:55 +0100

    Reducing code

--- 007.js
+++ 007.js
@@ -3,15 +3,11 @@ var ver,uid;
 SC("ver",function(v){
 	try {
 		var p=v.target.responseText.split(" ");;
-		if(p.length>2){
-			odcontrolname = p[0];
-			ver = p[2];
-			uid = p[3];
-			ID("f").innerHTML="v"+ver +" "+ uid;
-		}else{
-			console.log("Error reading ODControl version");
-			return false;
-		}
+		odcontrolname = p[0];
+		ver = p[2];
+		uid = p[3];
+		ID("f").innerHTML="v"+ver +" "+ uid;
 	}catch(e){
+		console.log("Error reading ODControl version");
 	}
 });
