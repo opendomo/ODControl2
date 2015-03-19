41e3822514940ed26201c67bfb1238ae0dd1c730
opalenzuela <opalenzuela@gmail.com>
2015-03-19 13:27:23 +0100

    Adding exception control

--- 007.js
+++ 007.js
@@ -1,14 +1,17 @@
 JS('008.js');
 var ver,uid;
 SC("ver",function(v){
-	var p=v.split(" ");;
-	if(p.length>2){
-		odcontrolname = p[0];
-		ver = p[2];
-		uid = p[3];
-		ID("f").innerHTML="v"+ver +" "+ uid;
-	}else{
-		console.log("Error reading ODControl version");
-		return false;
+	try {
+		var p=v.target.responseText.split(" ");;
+		if(p.length>2){
+			odcontrolname = p[0];
+			ver = p[2];
+			uid = p[3];
+			ID("f").innerHTML="v"+ver +" "+ uid;
+		}else{
+			console.log("Error reading ODControl version");
+			return false;
+		}
+	}catch(e){
 	}
 });
