80eeb3ddb570bb79dac00b4886ed03f44a1762e1
opalenzuela <opalenzuela@gmail.com>
2015-03-23 10:26:07 +0100

    EOL

--- 139.js
+++ 139.js
@@ -1,32 +1,31 @@
 function extractConfiguration_switch(res, i, pType, pName, pPage){
-
 	switch(pType){
 		case "DI":
-			res = res + "cfg " + pName + " di"+pPage+"\\n";
+			res = res + "cfg " + pName + " di" + pPage + EOL;
 			break;
 		case "DO":
-			res = res + "cfg " + pName + " do"+pPage+"\\n";
+			res = res + "cfg " + pName + " do" + pPage + EOL";
 			break;
 		case "DX":
-			res = res + "cfg " + pName + " x"+pPage+"\\n";
+			res = res + "cfg " + pName + " x" + pPage + EOL;
 			break;
 		case "AI":
 			var rangevals = ports[i][5].split("|");
-			res = res + "cfg " + pName + " "+ports[i][6] + pPage+"\\n";
-			res = res + "rng " + pName + " " + rangevals[0] + " " + rangevals[1] + "\\n";
+			res = res + "cfg " + pName + " " + ports[i][6] + pPage + EOL;
+			res = res + "rng " + pName + " " + rangevals[0] + " " + rangevals[1] + EOL;
 			break;
 		case "AO":
 			var rangevals = ports[i][5].split("|");
-			res = res + "cfg " + pName + " "+ports[i][6]+pPage+"\\n";
-			res = res + "rng " + pName + " " + rangevals[0] + " " + rangevals[1] + "\\n";
+			res = res + "cfg " + pName + " " + ports[i][6] + pPage + EOL;
+			res = res + "rng " + pName + " " + rangevals[0] + " " + rangevals[1] + EOL;
 			break;
 		case "VX":
-			res = res + "cfg " + pName + " x"+pPage+"\\n";
+			res = res + "cfg " + pName + " x" + pPage + EOL;
 			break;
 		case "XV":
 			break;
 		default:
-			res = res + "cfg " + pName + " d"+pPage+"\\n";
+			res = res + "cfg " + pName + " d" + pPage + "EOL";
 			res = res + vportExtractConfig(ports[i]);
 			break;
 	}
