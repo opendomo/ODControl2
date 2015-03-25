1f98ed7b0fb459844ce3d1fc07e4601a76238fba
opalenzuela <opalenzuela@gmail.com>
2015-03-25 14:53:52 +0100

    Adding more spice

--- 050.js
+++ 050.js
@@ -19,4 +19,4 @@ function SEL(x){
 	e.style.backgroundColor="#053f95";
 }
 for(var i=51;i<=65;i++)JS('0'+i+'.js');
-for(var i=20;i<=29;i++)CSS('0'+i+'.css');
+for(var i=20;i<=30;i++)CSS('0'+i+'.css');
