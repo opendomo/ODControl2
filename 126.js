d7d00fa3ac85b9d3ad674a15bba5b190d7b3e935
opalenzuela <opalenzuela@gmail.com>
2015-03-26 11:26:53 +0100

    Adding description

--- 126.js
+++ 126.js
@@ -1,10 +1,12 @@
 function loadTemplate(tpl){
 	try {
-		if (typeof tpl == "number") tpl = templates_cmd[tpl];
-		cmds=tpl.replace(EOL,";;").split(";;");
+		var comm = (typeof tpl == "number")?templates_cmd[tpl]:tpl; 
+		var desc = templates_desc[tpl];
+		cmds=comm.replace(EOL,";;").split(";;");
 		var total=cmds.length;
 		clearMenu();
 		addMenuItem("progressmenu","menuitem",LBL_LOAD,null);
+		addMenuItem("tdesc","menuitem","Nombre",desc);
 		addMenuItem("progressbarm","menuitem","",createProgressField("progress",0,total));
 		addMenuItem("progresstotalm","menuitem",LBL_TCM, cIF("progresstotal","text",total));
 		addMenuItem("progresscurrentm","menuitem",LBL_EXE, cIF("progresscurrent","text","0"));
