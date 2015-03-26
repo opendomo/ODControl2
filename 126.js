2da41b66514305d3a2c8ea491bcd51a03b4648ee
opalenzuela <opalenzuela@gmail.com>
2015-03-26 11:36:35 +0100

    using DOM

--- 126.js
+++ 126.js
@@ -5,8 +5,8 @@ function loadTemplate(tpl){
 		var total=cmds.length;
 		clearMenu();
 		addMenuItem("progressmenu","menuitem",LBL_LOAD,null);
-		addMenuItem("tnam","menuitem",LBL_NAM,templates[tpl]);
-		addMenuItem("tdesc","menuitem",LBL_DESC,templates_Desc[tpl]);
+		addMenuItem("tnam","menuitem",LBL_NAM+": ",CT(templates[tpl]));
+		addMenuItem("tdesc","menuitem",LBL_DESC+": ",CT(templates_Desc[tpl]));
 		addMenuItem("progressbarm","menuitem","",createProgressField("progress",0,total));
 		addMenuItem("progresstotalm","menuitem",LBL_TCM, cIF("progresstotal","text",total));
 		addMenuItem("progresscurrentm","menuitem",LBL_EXE, cIF("progresscurrent","text","0"));
