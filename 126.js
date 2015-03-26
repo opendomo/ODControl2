8801407f36535cef6e753b5f2b35b1582bc588df
opalenzuela <opalenzuela@gmail.com>
2015-03-26 11:30:40 +0100

    Adding name and description

--- 126.js
+++ 126.js
@@ -1,17 +1,16 @@
 function loadTemplate(tpl){
 	try {
 		var comm = (typeof tpl == "number")?templates_cmd[tpl]:tpl; 
-		var desc = templates_desc[tpl];
 		cmds=comm.replace(EOL,";;").split(";;");
 		var total=cmds.length;
 		clearMenu();
 		addMenuItem("progressmenu","menuitem",LBL_LOAD,null);
-		addMenuItem("tdesc","menuitem","Nombre",desc);
+		addMenuItem("tnam","menuitem",LBL_NAM,templates[tpl]);
+		addMenuItem("tdesc","menuitem",LBL_DESC,templates_Desc[tpl]);
 		addMenuItem("progressbarm","menuitem","",createProgressField("progress",0,total));
 		addMenuItem("progresstotalm","menuitem",LBL_TCM, cIF("progresstotal","text",total));
 		addMenuItem("progresscurrentm","menuitem",LBL_EXE, cIF("progresscurrent","text","0"));
 		addMenuItem("progresserrm","menuitem",LBL_ERR, cIF("progresserr","text","0"));
-
 		var ta=CE("textarea");
 		ta.setAttribute("id", "errlog");
 		ta.setAttribute("class", "err");
