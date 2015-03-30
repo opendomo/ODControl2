baf382878ad11b8a1c8a5565f885f274eeef9cc8
opalenzuela <opalenzuela@gmail.com>
2015-03-30 10:43:58 +0200

    Reorganizing templates

--- 126.js
+++ 126.js
@@ -2,29 +2,25 @@ function loadTemplate(tpl){
 	try {
 		var comm = tpl.commands;
 		cmds=comm.replace(EOL,";;").split(";;");
-		var total=cmds.length;
 		clearMenu();
 		addMenuItem("tnam","menuitem",LBL_NAM+": ",CT(tpl.name));
 		addMenuItem("tdesc","menuitem",LBL_DESC+": ",CT(tpl.desc));
+	} catch (e) {
+	}
+	addToolbarSaveCancelHelp(function(){
+		var total=cmds.length;
+		send_command("clr");
 		addMenuItem("progressmenu","menuitem",LBL_LOAD,null);
 		addMenuItem("progressbarm","menuitem","",createProgressField("progress",0,total));
 		addMenuItem("progresstotalm","menuitem",LBL_TCM, cIF("progresstotal","text",total));
 		addMenuItem("progresscurrentm","menuitem",LBL_EXE, cIF("progresscurrent","text","0"));
 		addMenuItem("progresserrm","menuitem",LBL_ERR, cIF("progresserr","text","0"));
-		var ta=CE("textarea");
-		ta.setAttribute("id", "errlog");
-		ta.setAttribute("class", "err");
+		var ta=CE("textarea","err","errlog");
 		ta.setAttribute("resize","none");
 		ta.setAttribute("readonly","");
 		addMenuItem("progresserrlog","menuitem",LBL_LOG,ta,null);
-	} catch (e) {
-	}
-	addToolbarSaveCancelHelp(function(){
-		send_command("clr");
 		curr_tpl_cmd=0;
 		failedcommands=0;
 		setTimeout(loadTemplateNextCommand,2000);		
 	},loadTemplates);
 }
-
-
