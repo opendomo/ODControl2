6b8ea1e43dfc47b8ab1620e1a8843ad6805bc135
opalenzuela <opalenzuela@gmail.com>
2015-04-01 09:39:46 +0200

    Formatting DTG

--- 059.js
+++ 059.js
@@ -7,24 +7,23 @@ function cWV(n,v){
 	SA(r,"class","hour");
 	SA(r,"value",m[2]+":"+m[3]+":"+m[4]);
 	AC(r,CT(v));
+	var s=cSF(n,m[1],["sun","mon","tue","wed","thu","fri","sat"],wdays);
+	SA(s,"class","wday");
 	var j=CE("input","txt",n); 
 	SA(j,"type","button");
 	SA(j,"value","Save");
 	SA(j,"class","save");
-	AC(j,CT(v));
-	var s=cSF(n,m[1],["sun","mon","tue","wed","thu","fri","sat"],wdays);
-	SA(s,"class","wday");
-	AC(j,CT(v));
+	AC(j,CT(v));	
 	var l=ID(n);
 	if(!l)l=CE("li","DD","li");
 	AC(l,CT(n));
-	AC(l,j);
 	AC(l,r);
 	AC(l,s);
+	AC(l,j);
 	j.addEventListener("click",function(){
-	var day=s.value;
-	var hour=r.value.replace(/:/g,"");
-	SC("set+"+n+"+"+day+hour,function(e){});
+		var day=s.value;
+		var hour=r.value.replace(/:/g,"");
+		SC("set+"+n+"+"+day+hour,function(e){});
 	});
 	return [r,l];
 }
